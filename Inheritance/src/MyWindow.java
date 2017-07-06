fc7165c9875aa148bb62467b78aeb0960f4ede68
Philip Limitone <plimitone@gmail.com>
2017-07-06 17:34:39 -0400

    inheritance from the tutorial
    inheritance from the tutorial

---
+++ Inheritance/src/MyWindow.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class MyWindow extends JFrame {
+	public static void main(String[] args) {
+		new MyWindow();
+		
+	}
+	
+	public void myWindow() {
+		setSize(500, 500);
+		setVisible(true);
+		setTitle("My Window");
+	}
+}
